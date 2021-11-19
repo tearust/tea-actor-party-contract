33c8a688fb569d020fec76abe5b1c15f920ac911
kevingzhang <kevin.zhang.canada@gmail.com>
2021-11-19 06:17:52 +0000

    correct handle request from replica

--- src/lib.rs
+++ src/lib.rs
@@ -12,47 +12,22 @@ use interface::{TOKEN_ID_TEA, Balance, Tsid,};
 use token_state::token_context::TokenContext;
 use vmh_codec::message::structs_proto::tokenstate::*;
 actor_handlers! {
-	codec::messaging::OP_DELIVER_MESSAGE => handle_message,
+	tea_codec::OP_ACTOR_EXEC_TXN => handle_txn_exec,
 	codec::core::OP_HEALTH_REQUEST => health
 }
-fn handle_message(msg: BrokerMessage) -> HandlerResult<Vec<u8>> {
-	debug!("simple-actor received deliver message, {:?}", &msg);
-
-	match handle_message_inner(msg) {
-		Ok(res) => Ok(res),
-		Err(e) => {
-			error!("simple-actor handle test task error {}", e);
-			Err(e)
-		}
-	}
-}
-
-fn handle_message_inner(msg: BrokerMessage) -> HandlerResult<Vec<u8>> {
-	let channel_parts: Vec<&str> = msg.subject.split('.').collect();
-	match &channel_parts[..] {
-		["tea", "system", "init"] => handle_system_init()?,
-		["replica", "txnexec"] => return handle_txn_exec(&msg.body),
-		 _ => (),
-	};
-	Ok(vec![])
-}
-
-fn handle_system_init() -> anyhow::Result<()> {
-	info!("tea party contract actor system init...");
-	Ok(())
-}
-
 fn helper_get_state_tsid()->HandlerResult<Tsid>{
 	let tsid_bytes: Vec<u8> = actor_statemachine::query_state_tsid()?;
 	let tsid: Tsid = bincode::deserialize(&tsid_bytes)?;
 	Ok(tsid)
 }
-
-fn handle_txn_exec(tsid_txn_bytes: &[u8])-> HandlerResult<Vec<u8>>{
-	let (tsid, txn_bytes):(Tsid, Vec<u8>) = bincode::deserialize(tsid_txn_bytes)?;
+fn handle_txn_exec(msg: BrokerMessage) -> HandlerResult<()> {
+	info!("enter handle_txn_exec");
+	let (tsid, txn_bytes):(Tsid, Vec<u8>) = bincode::deserialize(&msg.body)?;
+	info!("before SampleTxn der");
 	let sample_txn: SampleTxn = bincode::deserialize(&txn_bytes)?;
-	let base: Tsid = helper_get_state_tsid()?;
 	info!("decode the sample_txn {:?}", &sample_txn);
+	let base: Tsid = helper_get_state_tsid()?;
+	info!("base tsid is {:?}", &base);
 	let context_bytes = match sample_txn {
 		SampleTxn::Topup{acct, amt} =>{
 			info!("acct, amt: {:?}, {:?}", &acct, &amt);
@@ -110,10 +85,10 @@ fn handle_txn_exec(tsid_txn_bytes: &[u8])-> HandlerResult<Vec<u8>>{
 		_ =>Err(anyhow::anyhow!("Unhandled txn OP type"))?,
 	};
 	// let context: TokenContext = bincode::deserialize(&context_bytes)?;
-	let should_be_empty = actor_statemachine::commit(CommitRequest{
+	let _ = actor_statemachine::commit(CommitRequest{
 		ctx: context_bytes
 	})?;
-	Ok(should_be_empty)
+	Ok(())
 }
 fn health(_req: codec::core::HealthRequest) -> HandlerResult<()> {
 	info!("health call from simple actor");
