eef7e81c14915b2cde60f87fbf87b7c3b145a4cf
Yan Mingzhi <realraindust@gmail.com>
2021-11-24 14:59:41 +0800

    cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,16 @@
 #[macro_use]
 extern crate log;
-use serde::{Serialize, Deserialize};
 use bincode::Result as SerdeResult;
+use interface::{Balance, Tsid, TOKEN_ID_TEA};
+use prost::Message;
+use sample::SampleTxn;
+use serde::{Deserialize, Serialize};
+use tea_actor_utility::actor_statemachine;
+use token_state::token_context::TokenContext;
+use vmh_codec::message::structs_proto::tokenstate::*;
 use wascc_actor::prelude::codec::messaging::BrokerMessage;
 use wascc_actor::prelude::*;
 use wascc_actor::HandlerResult;
-use sample::SampleTxn;
-use prost::Message;
-use tea_actor_utility::actor_statemachine;
-use interface::{TOKEN_ID_TEA, Balance, Tsid,};
-use token_state::token_context::TokenContext;
-use vmh_codec::message::structs_proto::tokenstate::*;
 actor_handlers! {
 	codec::messaging::OP_DELIVER_MESSAGE => handle_message,
 	tea_codec::OP_ACTOR_EXEC_TXN => handle_txn_exec,
@@ -40,78 +40,75 @@ fn handle_system_init() -> anyhow::Result<()> {
 	Ok(())
 }
 
-
-fn helper_get_state_tsid()->HandlerResult<Tsid>{
+fn helper_get_state_tsid() -> HandlerResult<Tsid> {
 	let tsid_bytes: Vec<u8> = actor_statemachine::query_state_tsid()?;
 	let tsid: Tsid = bincode::deserialize(&tsid_bytes)?;
 	Ok(tsid)
 }
 fn handle_txn_exec(msg: BrokerMessage) -> HandlerResult<()> {
 	info!("enter handle_txn_exec");
-	let (tsid, txn_bytes):(Tsid, Vec<u8>) = bincode::deserialize(&msg.body)?;
+	let (tsid, txn_bytes): (Tsid, Vec<u8>) = bincode::deserialize(&msg.body)?;
 	info!("before SampleTxn der");
 	let sample_txn: SampleTxn = bincode::deserialize(&txn_bytes)?;
 	info!("decode the sample_txn {:?}", &sample_txn);
 	let base: Tsid = helper_get_state_tsid()?;
 	info!("base tsid is {:?}", &base);
 	let context_bytes = match sample_txn {
-		SampleTxn::Topup{acct, amt} =>{
+		SampleTxn::Topup { acct, amt } => {
 			let ctx = TokenContext::new(tsid, base, TOKEN_ID_TEA);
 			let ctx_bytes = bincode::serialize(&ctx)?;
 			let to: u32 = acct;
 			let amt: Vec<u8> = bincode::serialize(&amt)?;
-			actor_statemachine::topup(TopupRequest{
+			actor_statemachine::topup(TopupRequest {
 				ctx: ctx_bytes,
 				to,
 				amt,
 			})?
-		},
-		SampleTxn::TransferTea{from, to, amt} => {
+		}
+		SampleTxn::TransferTea { from, to, amt } => {
 			info!("TransferTea from to amt: {:?},{:?},{:?}", &from, &to, &amt);
 			let ctx = TokenContext::new(tsid, base, TOKEN_ID_TEA);
 			let ctx_bytes = bincode::serialize(&ctx)?;
 			let amt: Vec<u8> = bincode::serialize(&amt)?;
-			actor_statemachine::mov(MoveRequest{
+			actor_statemachine::mov(MoveRequest {
 				ctx: ctx_bytes,
 				from,
 				to,
 				amt,
 			})?
-		},
-		SampleTxn::PostMessage{from, ttl} => {
+		}
+		SampleTxn::PostMessage { from, ttl } => {
 			info!("PostMessage from ttl: {:?},{:?}", &from, &ttl);
-			let cost_please_add_app_logic_here = (ttl/1024u32) as Balance;
+			let cost_please_add_app_logic_here = (ttl / 1024u32) as Balance;
 			let to_please_add_app_logic_here_should_be_bonding_curve = 0u32;
 			let ctx = TokenContext::new(tsid, base, TOKEN_ID_TEA);
 			let ctx_bytes = bincode::serialize(&ctx)?;
 			let amt: Vec<u8> = bincode::serialize(&cost_please_add_app_logic_here)?;
-			actor_statemachine::mov(MoveRequest{
+			actor_statemachine::mov(MoveRequest {
 				ctx: ctx_bytes,
 				from,
 				to: to_please_add_app_logic_here_should_be_bonding_curve,
 				amt,
 			})?
-		},
-		SampleTxn::PrivateMessage{from, to, ttl} => {
+		}
+		SampleTxn::PrivateMessage { from, to, ttl } => {
 			info!("PrivateMessage from ttl: {:?},{:?},{:?}", &from, &to, &ttl);
-			let cost_please_add_app_logic_here = (ttl/1024u32) as Balance;
+			let cost_please_add_app_logic_here = (ttl / 1024u32) as Balance;
 			let to_please_add_app_logic_here_should_be_bonding_curve = 0u32;
 			let ctx = TokenContext::new(tsid, base, TOKEN_ID_TEA);
 			let ctx_bytes = bincode::serialize(&ctx)?;
 			let amt: Vec<u8> = bincode::serialize(&cost_please_add_app_logic_here)?;
-			actor_statemachine::mov(MoveRequest{
+			actor_statemachine::mov(MoveRequest {
 				ctx: ctx_bytes,
 				from,
 				to: to_please_add_app_logic_here_should_be_bonding_curve,
 				amt,
 			})?
-		},
-		_ =>Err(anyhow::anyhow!("Unhandled txn OP type"))?,
+		}
+		_ => Err(anyhow::anyhow!("Unhandled txn OP type"))?,
 	};
-	let res_commit_ctx_bytes = actor_statemachine::commit(CommitRequest{
-		ctx: context_bytes
-	})?;
-	if res_commit_ctx_bytes.is_empty(){
+	let res_commit_ctx_bytes = actor_statemachine::commit(CommitRequest { ctx: context_bytes })?;
+	if res_commit_ctx_bytes.is_empty() {
 		info!("*********  Commit succesfully. the ctx is empty. it is supposed to be empty");
 	}
 	Ok(())
